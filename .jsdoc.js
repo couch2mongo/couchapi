2b1c19c118e54484bbbdba89e26e1845d7f3471c
Toby Bessant <toby.bessant.1998@gmail.com>
2023-09-18 14:01:56 +0100

    feat: remaining everdeen views
    Co-authored-by: Lee Packham <me@leepackham.com>

---
+++ .jsdoc.js
@@ -0,0 +1,18 @@
+// Common JSDoc types for the JS views.
+
+/**
+ * @typedef CouchApiViewOptions
+ * @type {object}
+ * @property {boolean} reduce
+ * @property {boolean} group
+ * @property {number} group_level
+ * @property {boolean} include_docs
+ * @property {boolean} descending
+ * @property {never} limit
+ * @property {0} skip
+ * @property {any[]} start_key
+ * @property {any[]} end_key
+ * @property {string} startkey_docid
+ * @property {string} endkey_docid
+ * @property {string[]} keys
+ */
