a51e052e561ce39c3af2651275f68fc9b6f51db3
Lee Packham <me@leepackham.com>
2024-02-23 09:23:21 +0000

    chore: prepare for public repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// Copyright (c) 2024, Green Man Gaming Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // libcurl crashes on Sonoma unless CoreServices is linked in
 // See https://github.com/nabijaczleweli/cargo-update/issues/240 and https://github.com/mitsuhiko/rye/issues/428
 #[cfg_attr(target_os = "macos", link(name = "CoreServices", kind = "framework"))]
